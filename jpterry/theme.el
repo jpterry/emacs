1eeeeea26090d4091e64d92fa963b5cf925c473f
John Terry <jpterry@gmail.com>
2011-06-28 17:08:21 -0700

    Use wombat color theme for emacs

--- jpterry/theme.el
+++ jpterry/theme.el
@@ -1,7 +1,8 @@
 ;; Theme
 (add-to-list 'load-path "~/.emacs.d/vendor/color-theme-6.6.0/")
 (require 'color-theme)
+(color-theme-initialize)
 (setq color-theme-is-global t)
-(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
-(when window-system (color-theme-twilight))
-;(when window-system (color-theme-tangotango))
+(load-file "~/.emacs.d/vendor/color-theme-wombat.el")
+(when window-system (color-theme-wombat))
+
